425cacc3e0cd02b625bfaf00d0dc595caec60659
hikaru <alclimb@users.noreply.github.com>
2022-04-07 11:10:56 +0900

    refactor

--- 01.model-luminescence/src/main.ts
+++ 01.model-luminescence/src/main.ts
@@ -3,162 +3,137 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-interface Obj {
-  update: (delta: number) => void;
+/**
+ * Three.js関連のユーティリティモジュール
+ */
+export module ThreeUtils {
+  /**
+   * GLTFファイルを読み込む
+   */
+  export function loadGltf(url: string, onProgress?: (event: ProgressEvent) => void): Promise<GLTF> {
+    return new Promise((resolve, reject) => {
+      new GLTFLoader().load(url, resolve, onProgress, reject);
+    });
+  }
 }
 
-export module GameUtils {
-  export function addModel(scene: THREE.Scene) {
-    let mixer: THREE.AnimationMixer;
+const bloomParams = {
+  /** トーンマッピング: 露光量 */
+  exposure: 1.8,
 
-    new GLTFLoader().load(`static/PrimaryIonDrive.glb`,
-      function (gltf) {
-        // シーンに追加
-        scene.add(gltf.scene);
+  /** 発光エフェクト: 強さ */
+  bloomStrength: 3.0,
 
-        mixer = new THREE.AnimationMixer(gltf.scene);
-        mixer.clipAction(gltf.animations[0].optimize()).play();
-      },
-      () => {
-      },
-      errorEvent => {
-        console.log(`ERROR: `, errorEvent);
-      }
-    );
+  /** 発光エフェクト: 半径 */
+  bloomRadius: 1.2,
 
-    const update = (delta: number) => {
+  /** 発光エフェクト: 閾値 */
+  bloomThreshold: 0.0,
+};
+
+async function main() {
+  // DOMを取得
+  const appElement = document.querySelector<HTMLElement>(`#myApp`)!;
+
+  // モデルをロード
+  const model = await ThreeUtils.loadGltf(`static/PrimaryIonDrive.glb`);
+
+  // ライト(平行光)のセットアップ
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(7, 10, -2);
+  directionalLight.color.set(0xffffff);
+  directionalLight.castShadow = true; // ライトの影を有効
+  directionalLight.intensity = 0.3;
+  directionalLight.shadow.radius = 3.0;
+
+  // ライト(環境光)のセットアップ
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+
+  // 座標軸のセットアップ
+  const axes = new THREE.AxesHelper(25);
+
+  // シーンのセットアップ
+  const scene = new THREE.Scene();
+  scene.add(model.scene);
+  scene.add(directionalLight);
+  scene.add(ambientLight);
+  scene.add(axes);
+
+  // カメラのセットアップ
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    appElement.clientWidth / appElement.clientHeight,
+    0.01,
+    1000
+  );
+  camera.position.set(4, 2, 2);
+  camera.lookAt(scene.position);
+
+  // アニメーションミキサーのセットアップ
+  const mixer = new THREE.AnimationMixer(model.scene);
+  mixer.clipAction(model.animations[0].optimize()).play();
+
+  // レンダラーのセットアップ
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(appElement.clientWidth, appElement.clientHeight);
+  renderer.setClearColor(0x000000); // 背景色
+  renderer.shadowMap.enabled = true; // レンダラー：シャドウを有効にする
+  renderer.toneMapping = THREE.ReinhardToneMapping;
+  renderer.toneMappingExposure = Math.pow(bloomParams.exposure, 4.0);
+
+  // エフェクト: 通常レンダリング
+  const renderPass = new RenderPass(scene, camera);
+
+  // エフェクト: 発光エフェクト
+  const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(appElement.clientWidth, appElement.clientHeight),
+    bloomParams.bloomStrength,
+    bloomParams.bloomRadius,
+    bloomParams.bloomThreshold,
+  );
+
+  // エフェクトのセットアップ
+  const effectComposer = new EffectComposer(renderer);
+  effectComposer.addPass(renderPass);
+  effectComposer.addPass(bloomPass);
+  effectComposer.setSize(appElement.clientWidth, appElement.clientHeight);
+
+  // カメラコントローラーのセットアップ
+  const orbitControls = new OrbitControls(camera, renderer.domElement);
+  orbitControls.maxPolarAngle = Math.PI * 0.5;
+  orbitControls.minDistance = 1;
+  orbitControls.maxDistance = 100;
+  orbitControls.autoRotate = false;    // カメラの自動回転設定
+  orbitControls.autoRotateSpeed = 1.0; // カメラの自動回転速度
+
+  // DOMに追加
+  appElement.appendChild(renderer.domElement);
+
+  let lastTime = 0;
+
+  function animate(time: number = 0) {
+    requestAnimationFrame(time => animate(time));
+
+    if (lastTime !== undefined) {
+      const delta = (time - lastTime) / 1000;
+
+      // 
       mixer.update(delta);
-    }
-
-    return { update };
-  }
-}
-
-export class Game {
-  private camera: THREE.PerspectiveCamera;
-  private scene: THREE.Scene;
-  private renderer: THREE.WebGLRenderer;
-  private orbitControls: OrbitControls;
-  private effectComposer: EffectComposer;
-  private objs: Obj[] = [];
-  private lastTime = 0;
-
-  private bloomParams = {
-    /** トーンマッピング: 露光量 */
-    exposure: 1.8,
-
-    /** 発光エフェクト: 強さ */
-    bloomStrength: 3.0,
-
-    /** 発光エフェクト: 半径 */
-    bloomRadius: 1.2,
-
-    /** 発光エフェクト: 閾値 */
-    bloomThreshold: 0.0,
-  };
-
-  public constructor(element: HTMLElement) {
-    // シーンを初期化
-    this.scene = new THREE.Scene();
-
-    // モデルをロード
-    this.objs.push(GameUtils.addModel(this.scene));
-
-    // ライト：平行光
-    const directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(7, 10, -2);
-    directionalLight.color.set(0xffffff);
-    directionalLight.castShadow = true; // ライトの影を有効
-    directionalLight.intensity = 0.3;
-    directionalLight.shadow.radius = 3.0;
-    this.scene.add(directionalLight); // シーンに追加
-
-    // ライト：環境光
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
-    this.scene.add(ambientLight); // シーンに追加
-
-    // 座標軸を表示
-    const axes = new THREE.AxesHelper(25);
-    this.scene.add(axes); // シーンに追加
-
-    // カメラを初期化
-    this.camera = new THREE.PerspectiveCamera(
-      50,
-      element.clientWidth / element.clientHeight,
-      0.01,
-      1000
-    );
-    this.camera.position.set(4, 2, 2);
-    this.camera.lookAt(this.scene.position);
-
-    // レンダラーの初期化
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(element.clientWidth, element.clientHeight);
-    this.renderer.setClearColor(0x000000); // 背景色
-    this.renderer.shadowMap.enabled = true; // レンダラー：シャドウを有効にする
-    this.renderer.toneMapping = THREE.ReinhardToneMapping;
-    this.renderer.toneMappingExposure = Math.pow(this.bloomParams.exposure, 4.0);
-
-    // エフェクト設定
-    {
-      // 通常レンダリング
-      const renderPass = new RenderPass(this.scene, this.camera);
-
-      // 発光エフェクト
-      const bloomPass = new UnrealBloomPass(
-        new THREE.Vector2(element.clientWidth, element.clientHeight),
-        this.bloomParams.bloomStrength,
-        this.bloomParams.bloomRadius,
-        this.bloomParams.bloomThreshold,
-      );
-
-      this.effectComposer = new EffectComposer(this.renderer);
-      this.effectComposer.addPass(renderPass);
-      this.effectComposer.addPass(bloomPass);
-      this.effectComposer.setSize(element.clientWidth, element.clientHeight);
-    }
-
-    // カメラコントローラー設定
-    this.orbitControls = new OrbitControls(
-      this.camera,
-      this.renderer.domElement
-    );
-    this.orbitControls.maxPolarAngle = Math.PI * 0.5;
-    this.orbitControls.minDistance = 1;
-    this.orbitControls.maxDistance = 100;
-    this.orbitControls.autoRotate = false; // カメラの自動回転設定
-    this.orbitControls.autoRotateSpeed = 1.0; // カメラの自動回転速度
-
-    // DOM に追加
-    element.appendChild(this.renderer.domElement);
-  }
-
-  public animate(time: number = 0) {
-    requestAnimationFrame(time => this.animate(time));
-
-    if (this.lastTime !== undefined) {
-      const delta = (time - this.lastTime) / 1000;
-
-      // 演算結果を表示に更新
-      this.objs.forEach(obj => {
-        obj.update(delta);
-      });
 
       // カメラコントローラーを更新
-      this.orbitControls.update();
+      orbitControls.update();
     }
 
-    this.lastTime = time;
+    lastTime = time;
 
     // 描画する
-    this.effectComposer.render(time);
+    effectComposer.render(time);
   }
+
+  // アニメーションを開始
+  animate();
 }
 
-// DOMを取得
-const appElement = document.querySelector<HTMLElement>(`#myApp`)!;
-
-const game = new Game(appElement);
-game.animate();
+main();
