9160381e3a617d25f316d7b0774d5db2f30c0a22
hikaru <alclimb@users.noreply.github.com>
2022-04-16 06:42:44 +0900

    refactor:

--- 01.text-2d/src/main.ts
+++ 01.text-2d/src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { MeshText2D, textAlign } from "three-text2d";
 import * as chroma from "chroma-js";
 import TWEEN from "@tweenjs/tween.js";
@@ -26,160 +27,125 @@ export module utils {
   }
 }
 
-export class Game {
-  /** THREE: カメラ */
-  private camera: THREE.PerspectiveCamera;
+async function main(element: HTMLElement) {
+  const width = element.offsetWidth;
+  const height = element.offsetHeight;
 
-  /** THREE: シーン */
-  private scene: THREE.Scene;
+  // ライト：平行光
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(7, 10, -2);
+  directionalLight.color.set(0xffffff);
+  directionalLight.intensity = 0.3;
+  directionalLight.shadow.radius = 3.0;
+  directionalLight.castShadow = true; // ライトの影を有効
 
-  /** THREE: レンダラー */
-  private renderer: THREE.WebGLRenderer;
+  // ライト：環境光
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
 
-  /** THREE: 操作コントローラー */
-  private orbitControls: OrbitControls;
+  //   // 3Dテキストを追加
+  //   {
+  //     this.titleTextMesh = new MeshText2D(`100 DAYS OF CODE\nDAY 9`, {
+  //       align: textAlign.bottom,
+  //       font: `120px Arial`,
+  //       antialias: true,
+  //       shadowColor: `#909090`,
+  //       shadowBlur: 4,
+  //       shadowOffsetX: 2,
+  //       shadowOffsetY: 2,
+  //       lineHeight: 1.0,
+  //     });
+  //     this.titleTextMesh.position.set(0, 0, -0.3);
+  //     this.titleTextMesh.rotateX(utils.toRadian(-90));
+  //     this.titleTextMesh.scale.set(0.001, 0.001, 0.001);
 
-  /** THREE: テキストMesh */
-  private titleTextMesh: MeshText2D;
+  //     // シーンに追加
+  //     this.scene.add(this.titleTextMesh);
+  //   }
 
-  /** THREE: テキストMesh */
-  private colorTextMesh: MeshText2D;
+  // // 3Dテキストを追加
+  // {
+  //   this.colorTextMesh = new MeshText2D(``, {
+  //     align: textAlign.center,
+  //     font: `120px monospace`,
+  //     antialias: true,
+  //     shadowColor: `#909090`,
+  //     shadowBlur: 4,
+  //     shadowOffsetX: 2,
+  //     shadowOffsetY: 2,
+  //   });
+  //   this.colorTextMesh.position.set(0.0, 0, 0.0);
+  //   this.colorTextMesh.rotateX(utils.toRadian(-90));
+  //   this.colorTextMesh.scale.set(0.004, 0.004, 0.004);
 
-  public constructor(element: HTMLElement) {
+  //   // シーンに追加
+  //   this.scene.add(this.colorTextMesh);
+  // }
+
+  // シーンを初期化
+  const scene = new THREE.Scene();
+  scene.add(directionalLight);
+  scene.add(ambientLight);
+
+  // カメラを初期化
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    width / height,
+    0.01,
+    1000
+  );
+  camera.position.set(1, 1, 1);
+  camera.lookAt(scene.position);
+
+  // レンダラーの初期化
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(width, height);
+  renderer.shadowMap.enabled = true; // レンダラー：シャドウを有効にする
+  // renderer.toneMapping = THREE.ReinhardToneMapping;
+  // renderer.toneMappingExposure = Math.pow(1.0, 4.0);
+
+  // カメラコントローラー設定
+  const orbitControls = new OrbitControls(camera, renderer.domElement);
+  orbitControls.maxPolarAngle = Math.PI * 0.5;
+  orbitControls.minDistance = 1;
+  orbitControls.maxDistance = 100;
+  orbitControls.autoRotate = false; // カメラの自動回転設定
+  orbitControls.autoRotateSpeed = 1.0; // カメラの自動回転速度
+
+  // Windowサイズ変更イベントハンドラ
+  const onWindowResize = () => {
     const width = element.offsetWidth;
     const height = element.offsetHeight;
 
-    // シーンを初期化
-    this.scene = new THREE.Scene();
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
 
-    // カメラを初期化
-    {
-      this.camera = new THREE.PerspectiveCamera(
-        50,
-        width / height,
-        0.01,
-        1000
-      );
-      this.camera.position.set(1, 1, 1);
-      this.camera.lookAt(this.scene.position);
-    }
-
-    // レンダラーの初期化
-    {
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
-      this.renderer.setSize(width, height);
-      this.renderer.shadowMap.enabled = true; // レンダラー：シャドウを有効にする
-      // this.renderer.toneMapping = THREE.ReinhardToneMapping;
-      // this.renderer.toneMappingExposure = Math.pow(1.0, 4.0);
-    }
-
-    // カメラコントローラー設定
-    {
-      this.orbitControls = new OrbitControls(
-        this.camera,
-        this.renderer.domElement
-      );
-      this.orbitControls.maxPolarAngle = Math.PI * 0.5;
-      this.orbitControls.minDistance = 1;
-      this.orbitControls.maxDistance = 100;
-      this.orbitControls.autoRotate = false; // カメラの自動回転設定
-      this.orbitControls.autoRotateSpeed = 1.0; // カメラの自動回転速度
-    }
-
-    // ライト：平行光
-    {
-      const directionalLight = new THREE.DirectionalLight(0xffffff);
-      directionalLight.position.set(7, 10, -2);
-      directionalLight.color.set(0xffffff);
-      directionalLight.intensity = 0.3;
-      directionalLight.shadow.radius = 3.0;
-      directionalLight.castShadow = true; // ライトの影を有効
-
-      // シーンに追加
-      // this.scene.add(directionalLight);
-    }
-
-    // ライト：環境光
-    {
-      const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-
-      // シーンに追加
-      this.scene.add(ambientLight);
-    }
-
-    // 3Dテキストを追加
-    {
-      this.titleTextMesh = new MeshText2D(`100 DAYS OF CODE\nDAY 9`, {
-        align: textAlign.bottom,
-        font: `120px Arial`,
-        antialias: true,
-        shadowColor: `#909090`,
-        shadowBlur: 4,
-        shadowOffsetX: 2,
-        shadowOffsetY: 2,
-        lineHeight: 1.0,
-      });
-      this.titleTextMesh.position.set(0, 0, -0.3);
-      this.titleTextMesh.rotateX(utils.toRadian(-90));
-      this.titleTextMesh.scale.set(0.001, 0.001, 0.001);
-
-      // シーンに追加
-      this.scene.add(this.titleTextMesh);
-    }
-
-    // 3Dテキストを追加
-    {
-      this.colorTextMesh = new MeshText2D(``, {
-        align: textAlign.center,
-        font: `120px monospace`,
-        antialias: true,
-        shadowColor: `#909090`,
-        shadowBlur: 4,
-        shadowOffsetX: 2,
-        shadowOffsetY: 2,
-      });
-      this.colorTextMesh.position.set(0.0, 0, 0.0);
-      this.colorTextMesh.rotateX(utils.toRadian(-90));
-      this.colorTextMesh.scale.set(0.004, 0.004, 0.004);
-
-      // シーンに追加
-      this.scene.add(this.colorTextMesh);
-    }
-
-    // Windowサイズ変更イベントハンドラ
-    const onWindowResize = () => {
-      const width = element.offsetWidth;
-      const height = element.offsetHeight;
-
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-
-      this.renderer.setSize(width, height);
-    }
-
-    // DOMに追加
-    element.appendChild(this.renderer.domElement);
-
-    // DOMイベントの登録
-    window.addEventListener(`resize`, onWindowResize, false);
+    renderer.setSize(width, height);
   }
 
-  public animate() {
+  // DOMに追加
+  element.appendChild(renderer.domElement);
+
+  // DOMイベントの登録
+  window.addEventListener(`resize`, onWindowResize, false);
+
+  animate();
+  
+  function animate() {
     // Tween: 色の変更アニメーション
     {
       // アニメーション変化時の処理
       const onUpdate = (val: number[]) => {
         const color = chroma.rgb(val[0], val[1], val[2]);
 
-        // テキストの色を変更
-        this.titleTextMesh.fillStyle = color.darken(2.0).hex();
+        // // テキストの色を変更
+        // this.titleTextMesh.fillStyle = color.darken(2.0).hex();
 
-        // テキストの色を変更
-        this.colorTextMesh.fillStyle = color.brighten(3.0).hex();
-        this.colorTextMesh.text = color.hex();
+        // // テキストの色を変更
+        // this.colorTextMesh.fillStyle = color.brighten(3.0).hex();
+        // this.colorTextMesh.text = color.hex();
 
         // 背景を変更
-        this.renderer.setClearColor(color.hex());
+        renderer.setClearColor(color.hex());
       };
 
       // Tween: アニメーション生成
@@ -203,7 +169,7 @@ export class Game {
     let lastTime = 0;
 
     // アニメーションループを開始
-    this.renderer.setAnimationLoop((time: number) => {
+    renderer.setAnimationLoop((time: number) => {
       // deltaを算出
       const delta = (time - lastTime) / 1000;
 
@@ -211,18 +177,18 @@ export class Game {
       TWEEN.update(time);
 
       // カメラコントローラーを更新
-      this.orbitControls.update();
+      orbitControls.update();
 
       // 描画する
-      this.renderer.render(this.scene, this.camera);
+      renderer.render(scene, camera);
 
       lastTime = time;
     });
   }
 }
 
-// DOMを取得
-const appElement = document.querySelector<HTMLElement>(`#myApp`)!;
-
-const game = new Game(appElement);
-game.animate();
+window.onload = function () {
+  // DOMを取得
+  const appElement = document.querySelector<HTMLElement>(`#myApp`)!;
+  main(appElement);
+}
